da2a845b38eb1afc1f9114224b2db9e19bae655c
minbee <101392857+alsswl@users.noreply.github.com>
2023-01-17 21:26:27 +0900

    [Silver III] Title: N과 M (1), Time: 260 ms, Memory: 22492 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,64 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+	
+	static boolean[] visit;
+	static int n;
+	static int m;
+	static StringBuilder sb;
+	
+	static int[] arr;
+	
+	static void print() {
+		for(int i = 0;i<m;i++) {
+			sb.append(arr[i]);
+			sb.append(" ");
+		}
+		sb.append("\n");
+	}
+	
+	static void re(int k) {
+		
+		if(k == m) {
+			print();
+		}
+		else {
+			for(int i = 1;i<=n;i++) {
+			if(visit[i] == false) {
+				arr[k] = i;
+				visit[i] = true;
+			    re(k+1);
+			    visit[i] = false;
+			}
+			
+		}
+		}
+		
+	}
+	
+	
+	
+    public static void main(String[] args) throws IOException{
+    	
+    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        visit = new boolean[n+1];
+        
+        arr = new int[m];
+        
+        re(0);
+        
+        System.out.println(sb);
+        	
+        	
+    }
+}
