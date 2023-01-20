2643accc0211cf20e60543d190f07c008cee118d
minbee <101392857+alsswl@users.noreply.github.com>
2023-01-20 21:38:14 +0900

    [Silver III] Title: N과 M (7), Time: 616 ms, Memory: 119664 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+	
+	static boolean[] visit;
+	static int n;
+	static int m;
+	static StringBuilder sb;
+	
+	static int[] arr;
+	static int[] arr2;
+	
+	
+	static void print() {
+		for(int i = 0;i<m;i++) {
+			sb.append(arr2[arr[i]]);
+			sb.append(" ");
+		}
+		sb.append("\n");
+	}
+	
+	static void re(int k) {
+		
+		if(k == m) {
+			print();
+		}
+		else {
+			for(int i = 1;i<=n;i++) {
+				
+					arr[k] = i;
+					//visit[i] = true;
+				    re(k+1);
+				    //visit[i] = false;
+				
+				
+			
+		}
+		}
+		
+	}
+	
+	
+	
+    public static void main(String[] args) throws IOException{
+    	
+    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        visit = new boolean[n+1];
+        
+        arr = new int[m];
+        arr2 = new int[n+1];
+        
+       
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i =1;i<=n;i++) {
+        	arr2[i] = Integer.parseInt(st.nextToken());
+        }
+         Arrays.sort(arr2);
+        re(0);
+        
+        System.out.println(sb);
+        	
+        	
+    }
+}
