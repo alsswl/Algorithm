f17f0ea74a579f6804e3c40434501be684b222b0
minbee <101392857+alsswl@users.noreply.github.com>
2023-02-07 23:01:41 +0900

    [Bronze V] Title: A+B - 7, Time: 176 ms, Memory: 15008 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+	
+	
+	
+    public static void main(String[] args) throws IOException{
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 1;i<=n;i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		
+    		System.out.printf("Case #%d: %d\n",i,a+b);
+    	}
+    	
+    	}
+    	
+        
+    }
+
