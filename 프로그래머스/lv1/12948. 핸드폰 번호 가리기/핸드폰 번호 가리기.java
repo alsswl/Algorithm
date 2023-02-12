43e93fd7b277080708f8fc7baccef3dbb288bff5
minbee <101392857+alsswl@users.noreply.github.com>
2023-02-12 17:59:58 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        
+        char[] arr = phone_number.toCharArray();
+        
+        for(int i = 0;i<arr.length-4;i++){
+            arr[i] = '*';
+        }
+        String answer = String.valueOf(arr);
+        return answer;
+    }
+    
+}
