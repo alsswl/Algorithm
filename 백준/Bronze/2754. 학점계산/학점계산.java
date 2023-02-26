edc5a9f59bf1f2ebddc1f53616b2a0efee3e9eba
minbee <101392857+alsswl@users.noreply.github.com>
2023-02-27 00:01:44 +0900

    [Bronze V] Title: 학점계산, Time: 120 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+	
+	
+	
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	String ss = br.readLine();
+    	
+    	if(ss.equals("A+")) System.out.println(4.3);
+    	if(ss.equals("A0")) System.out.println(4.0);
+    	if(ss.equals("A-")) System.out.println(3.7);
+    	if(ss.equals("B+")) System.out.println(3.3);
+    	if(ss.equals("B0")) System.out.println(3.0);
+    	if(ss.equals("B-")) System.out.println(2.7);
+    	if(ss.equals("C+")) System.out.println(2.3);
+    	if(ss.equals("C0")) System.out.println(2.0);
+    	if(ss.equals("C-")) System.out.println(1.7);
+    	if(ss.equals("D+")) System.out.println(1.3);
+    	if(ss.equals("D0")) System.out.println(1.0);
+    	if(ss.equals("D-")) System.out.println(0.7);
+    	if(ss.equals("F")) System.out.println(0.0);
+    	
+    	
+}
+    
+}
+    
+
+
