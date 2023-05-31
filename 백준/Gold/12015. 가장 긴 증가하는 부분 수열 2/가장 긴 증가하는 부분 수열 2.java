8849cf8334c04e5525ca24358c75502dd943e09d
minbee <101392857+alsswl@users.noreply.github.com>
2023-05-31 22:34:35 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 536 ms, Memory: 95988 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,57 +1,50 @@
-import java.util.Scanner;
- 
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		int[] seq = new int[N];
-		int[] LIS = new int[N];
- 
- 
-		for (int i = 0; i < N; i++) {
-			seq[i] = in.nextInt();
-		}
- 
-		
-		LIS[0] = seq[0];
-		int lengthOfLIS = 1;
-		
-		for (int i = 1; i < N; i++) {
- 
-			int key = seq[i];
-			
-			
-			if (LIS[lengthOfLIS - 1] < key) {
-				lengthOfLIS++;
-				LIS[lengthOfLIS - 1] = key;
-			} 
-			else {
-				
-				int lo = 0;
-				int hi = lengthOfLIS;
-				while (lo < hi) {
-					int mid = (lo + hi) / 2;
-					
-					if(LIS[mid] < key) {
-						lo = mid + 1;
-					}
-					else {
-						hi = mid;
-					}
- 
-				}
-			
-				
-				LIS[lo] = key;
-			
-			}
-			
-		}
-		
-		System.out.println(lengthOfLIS);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int last = 1;
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+
+            if (dp[last - 1] < key) {
+                last++;
+                dp[last - 1] = key;
+            } else {
+                int left = 0;
+                int right = last;
+
+                while (left < right) {
+                    int mid = (left + right) / 2;
+
+                    if (dp[mid] < key) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+
+                dp[left] = key;
+            }
+        }
+
+        System.out.println(last);
+    }
 }
